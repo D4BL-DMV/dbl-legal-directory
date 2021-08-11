1f217d57ba237396b8775915209787b77dc1c1be
mcoirad <mcoirad@yahoo.com>
2021-08-11 21:18:11 +0000

    site update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js'
 )
 
-importScripts('./precache-manifest.7b0c78d4462f258cc82d5ad5ebb68e9e.js')
+importScripts('./precache-manifest.2cf75c58ade9ba501c202388e2079430.js')
 
 workbox.clientsClaim()
 
