db7020a5bec46e7e05342ab01081e8c1fe8f07ab
mcoirad <mcoirad@yahoo.com>
2021-11-02 04:28:37 +0000

    added aria landmarks

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -5,16 +5,19 @@ import './footer.scss'
 export default function Footer() {
   return (
     <div className="footer content panel dark">
-      <a
-        href="http://www.https://d4bl.org/"
-        alt="Data 4 Black Lives"
-        className="ebdsa-logo"
-      >
-        Data 4 Black Lives
-      </a>
-      <a href="http://www.https://d4bl.org/" alt="Data 4 Black Lives">
-        Created by Law 4 Black Lives DC in collaboration with Data 4 Black Lives DMV
-      </a>
+      <footer>
+        <a
+          href="http://www.https://d4bl.org/"
+          alt="Data 4 Black Lives"
+          className="ebdsa-logo"
+        >
+          Data 4 Black Lives
+        </a>
+        <a href="http://www.https://d4bl.org/" alt="Data 4 Black Lives">
+          Created by Law 4 Black Lives DC in collaboration with Data 4 Black
+          Lives DMV
+        </a>
+      </footer>
     </div>
   )
 }
