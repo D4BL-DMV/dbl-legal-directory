40a48057550c371e83748ce96df4b84cdc3d9e22
kev-8 <62524028+kev-8@users.noreply.github.com>
2021-11-04 09:42:41 -0400

    Update socialLinks.js
    updated twitter link to l4bl dc twitter page

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -4,7 +4,7 @@ import twitterIcon from '../images/twitter.svg'
 const LINKS = [
   {
     label: 'Twitter',
-    url: 'https://twitter.com/Data4BlackLives',
+    url: 'https://twitter.com/L4BLDC',
     icon: twitterIcon
   }
   // { label: 'Facebook', url: 'https://facebook.com', icon: facebookIcon }
