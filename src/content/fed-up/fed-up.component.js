0d1de57490182779285ca853c5edcbfeea25c6c6
kev-8 <62524028+kev-8@users.noreply.github.com>
2021-11-06 15:28:33 -0400

    Update fed-up.component.js

--- src/content/fed-up/fed-up.component.js
+++ src/content/fed-up/fed-up.component.js
@@ -9,22 +9,19 @@ const FEDUP = function(props) {
       <div className="content panel light">
         <div className="inner">
           <p>
-            
             Law4BlackLives DC seeks to eradicate white supremacy and 
             anti-Blackness by leveraging our collective skills and experiences 
             as lawyers, legal professionals, and Black people to support 
             Black-led community efforts in the struggle for liberation and 
             justice in metropolitan Washington, DC.
-    
+          </p>
+          <p>
             Data for Black Lives is a movement of activists, organizers, and
             mathematicians committed to the mission of using data science to
             create concrete and measurable change in the lives of Black people.
           </p>
-
-          </p>
           <Button href="https://d4bl.org/"> Learn More about D4BL </Button>
           <Button href="http://www.law4blacklivesdc.com/"> Learn More about L4BL DC </Button>
-          </p>
         </div>
       </div>
     </div>
